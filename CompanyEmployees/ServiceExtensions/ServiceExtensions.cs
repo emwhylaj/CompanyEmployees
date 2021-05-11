9cbb394ebade28f5d4d2944d0d1e56efbfc0efcf
Muyiwa Olalekan <muyiwa.olalekan@thebulb.africa>
2021-05-11 12:07:23 +0100

    content negotiation and implementation of Custom Formatter

--- CompanyEmployees/ServiceExtensions/ServiceExtensions.cs
+++ CompanyEmployees/ServiceExtensions/ServiceExtensions.cs
@@ -33,5 +33,8 @@ namespace CompanyEmployees.ServiceExtensions
 
         public static void ConfigureRepositoryManager(this IServiceCollection services) =>
              services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+            builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
     }
 }
